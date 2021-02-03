545ec225f7c2be863e68c6ebcda34fa6ba019233
naveen rajan <rajanice1996@gmail.com>
2021-02-03 13:20:42 +0530

    answer done!!

---
+++ euler_3.py
@@ -0,0 +1,18 @@
+import math
+
+
+def is_prime(n):
+    for i in range(2, int(math.sqrt(n))):
+        if n % i == 0:
+            return False
+    return True
+
+
+N = 600851475143
+
+for i in range(int(math.sqrt(N)), 2, -1):
+    if N % i == 0:
+        if is_prime(i):
+            print(i)
+            break
+
