cb9b6ad4699333a2188b67783de44064b7387de8
naveen rajan <rajanice1996@gmail.com>
2021-08-31 14:52:59 +0530

    Update euler_004.py

--- euler_004.py
+++ euler_004.py
@@ -1,18 +1,19 @@
-def is_pal(n):
-    s1 = str(n)
-    s2 = s1[::-1]
-    if s1 == s2:
+def is_palindrome(n):
+    # returns a bool true if given number is a palindrome
+    # else returns false
+    
+    string1 = str(n)            # converts the number to string
+    string2 = string1[::-1]     # reverses the string and compares
+    if string1 == string2:
         return True
     return False
 
-
 ans = 0
-
 for i in range(100, 1000):
     for j in range(i, 1000):
         x = i*j
-        if is_pal(x):
-            if x > ans:
+        if is_palindrome(x):
+            if x > ans:         # checks if larger and updates
                 ans = x
 
 print(ans)
