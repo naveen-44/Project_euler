710630ec63e874207ba809049fcb8f9c9ff7610f
naveen rajan <rajanice1996@gmail.com>
2021-08-31 15:07:53 +0530

    Update euler_005.py

--- euler_005.py
+++ euler_005.py
@@ -4,9 +4,8 @@ import math
 def is_prime(n):
     if n < 2:
         return False
-
     for i in range(2, int(math.sqrt(n))+1):
-        if n % i == 0:
+        if n%i == 0:
             return False
     return True
 
@@ -15,11 +14,8 @@ N = 20
 ans = 1
 for i in range(N+1):
     if is_prime(i):
-        # print("i", i)
         x = i
         while x*i <= N:
             x = x * i
-        # print("x", x)
         ans = ans * x
-
 print(ans)
