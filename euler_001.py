e1f2b4d1ace40977e682e75120e5ea0030a4a91b
naveen rajan <rajanice1996@gmail.com>
2021-08-31 12:26:16 +0530

    Update euler_001.py

--- euler_001.py
+++ euler_001.py
@@ -2,13 +2,17 @@
 # We find the sum of all the multiples of 3, 5 and 15 seperately
 # Then we subract the sum of 15 from the sum of the sum of 3 and 5
 
+def sumOfAP(a,d,n):
+  return int(n/2*(2*a + (n-1)*d)) 
+
+
 N = 999
-d1, d2, d3 = 3, 5, 15                         # Difference between each term of AP
-a1, a2, a3 = 3, 5, 15                         # Starting term of the AP
-n1, n2, n3 = int(N/d1), int(N/d2), int(N/d3)  # number of terms under 1000 as per question
+d1, d2, d3 = 3, 5, 15               # Difference between each term of AP
+a1, a2, a3 = 3, 5, 15               # Starting term of the AP
+n1, n2, n3 = N//d1, N//d2, N//d3    # number of terms under 1000 as per question
 
-s1 = int(n1/2*(2*a1 + (n1-1)*d1))             # sum of all the multiples of 3
-s2 = int(n2/2*(2*a2 + (n2-1)*d2))             # sum of all the multiples of 5
-s3 = int(n3/2*(2*a3 + (n3-1)*d3))             # sum of all the multiples of 15
+s1 = sumOfAP(a1,d1,n1)              # sum of all the multiples of 3
+s2 = sumOfAP(a2,d2,n2)              # sum of all the multiples of 5
+s3 = sumOfAP(a3,d3,n3)              # sum of all the multiples of 15
 
-print(s1+s2-s3)                               # requires result
+print(s1+s2-s3)                     # required result
