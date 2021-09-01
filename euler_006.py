ee0dfc1bc8218eaa9a35349920a316798c7f01f4
naveen rajan <rajanice1996@gmail.com>
2021-09-01 10:47:49 +0530

    Update euler_006.py

--- euler_006.py
+++ euler_006.py
@@ -1,10 +1,8 @@
 def sum_square(n):
-    return int(n * (n + 1) * (2*n + 1) / 6)
-
+    return int(n*(n + 1)*(2*n + 1)/6)
 
 def square_sum(n):
-    return int(n * n * (n + 1) * (n + 1) / 4)
-
+    return int((n*(n + 1))/2)**2
 
 N = 100
 print(square_sum(N) - sum_square(N))
