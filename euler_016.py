d359db1762d8110e881be797ce6f7db28ae84597
naveen rajan <rajanice1996@gmail.com>
2021-09-01 11:37:55 +0530

    Update euler_016.py

--- euler_016.py
+++ euler_016.py
@@ -1,10 +1,19 @@
-import math
-n = 1000
-p = int(math.pow(2, n))
-p = str(p)
+def fastPower(a,b):
+    # calculates powers in O(Logn) time
+    if a == 1 or b == 0:
+        return 1
+    if b == 1:
+        return a
+    # call a recursive function for the remaining half
+    t = fastPower(a,b//2)
+    res = t*t
+    if b%2 == 1:
+        res = res*a
+    return res
 
-res = 0
-for d in p:
-    res = int(d) + res
 
-print(res)
+number = fastPower(2,1000)
+digitSum = 0
+for s in str(number):
+    digitSum += int(s)
+print(digitSum)
