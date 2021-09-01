ac62575e22583f062fc9649f8731df70621462e5
naveen rajan <rajanice1996@gmail.com>
2021-09-01 11:07:25 +0530

    Update euler_009.py

--- euler_009.py
+++ euler_009.py
@@ -1,9 +1,12 @@
 import math
 
+flag = False
 for a in range(1, 1000):
-    for b in range(a+1, 1000):
-        c = math.sqrt(a*a + b*b)
-        if c == int(c):
-            if a+b+c == 1000:
-                print(int(a * b * c))
+    if not flag:                        # found answer or not
+        for b in range(a+1, 1000):
+            c = math.sqrt(a*a + b*b)    
+            if a+b+c >1000:             # no need to loop further
                 break
+            if a+b+c == 1000:           # no need to run further coz only one solution
+                print(int(a*b*c))
+                flag = True
